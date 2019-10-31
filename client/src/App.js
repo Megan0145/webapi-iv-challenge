75a757633f1d93f6334e6a62cd9a54cd0269f02c
Megan Ennis <Megan@Megans-iMac-9.local>
2019-10-31 14:10:13 +0000

    Consuming data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.get('/api/users')
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
